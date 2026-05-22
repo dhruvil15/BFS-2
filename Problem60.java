78a8bb54d8f4e6fc3358271a973c413571a01aee
dhruvil15 <76967484+dhruvil15@users.noreply.github.com>
2026-05-21 20:09:32 -0400

    Complete BFS-2

---
+++ Problem60.java
@@ -0,0 +1,32 @@
+// Time Complexity : O(N)
+// Space Complexity : O(h)
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                if (size-1 == i) {
+                    result.add(curr.val);
+                }
+
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+        }
+
+        return result;
+    }
+}
